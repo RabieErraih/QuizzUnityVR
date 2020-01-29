d9a7fb51442c480ce0ad14e4b33b561e4e0a58a5
Erraih <rabieerraih@gmail.com>
2020-01-29 16:31:16 +0100

    Initial Commit

---
+++ Assets/Scripts/vb_anim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbBtnObj;
+    public TextMesh label_result;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BTN PRESSED");
+        label_result.text = "C'est correct! \n Indice:";
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BTN RELEASED");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Answer1Btn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/vb_anim2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vb_anim2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+    public TextMesh label_result;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("BTN PRESSED");
+        label_result.text = "C'est FAUX!";
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("Answer2Btn");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
